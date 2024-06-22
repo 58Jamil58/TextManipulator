ee6794a98c4a57d73421add1724bb4ef4747dfb3
58Jamil58 <136060025+58Jamil58@users.noreply.github.com>
2024-06-22 14:07:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alert from './components/Alert';
+
+
+function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert('')
+    }, 2000);
+  }
+  let toggleMode=()=>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.backgroundColor='black';
+      showAlert("Dark Mode has been enabled","success")
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode has been enabled","success")
+    }
+  }
+  return (
+    <>
+
+      <Navbar title = 'TextManipulator' mode = {mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+        
+        <TextForm showAlert = {showAlert} heading = 'Enter text to analyze below' mode = {mode}/>
+        {/* <About/> */}
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,56 @@
+import React,{useState} from 'react'
+import Proptypes from 'prop-types'
+
+export default function TextForm(props) {
+    const handleUpCase = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase!","success");
+    }
+    const handleLoCase = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase!","success");
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+    const handleOnCopy = ()=>{
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied!","success");
+    }
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[' ']+/);
+        setText(newText.join(" "))
+        props.showAlert("Extra Space Removed!","success");
+    }
+    const handleOnClear = ()=>{
+        let newText = '';
+        setText(newText)
+        props.showAlert("Cleared!","success");
+    }
+    const [text,setText] = useState('');
+    return (
+        <>
+            <div className="mb-3" style={{color:props.mode==='dark'?'white':'black'}}>
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" value = {text}  onChange={handleOnChange} style={{backgroundColor:props.mode==='light'?'white':'#212529',color:props.mode==='dark'?'white':'black'}}  id="myBox" rows="10"></textarea>
+            </div>  
+            <button className="btn btn-primary mx-2" onClick={handleUpCase}>Convert To Uppercaser</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoCase}>Convert To LowerCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleOnCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Space</button>
+            <button className="btn btn-primary mx-2" onClick={handleOnClear}>Clear Text</button>
+            <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+                <h2>Your Text Summary</h2>
+                <p>{text.length==0?0:text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008*(text.length==0?0:text.split(" ").length)} minutes to read these words</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:'Please enter something in the TextBox to preview here'}</p>
+
+            </div>
+    </>
+  )
+}
